beea38e24452d8478cbfe3d466c1066c2ea16fa3
mdonthireddy <donthireddy@yahoo.com>
2017-11-05 19:23:29 -0500

    Update cycle_tut.js

--- cycle_tut.js
+++ cycle_tut.js
@@ -1,4 +1,15 @@
 // Typed From Cyclejs video tutorial
+
+// Also see this 92 line cycle-core implementation
+// https://gist.github.com/staltz/0e3cdf20a0b7696e7943
+
+// Cycle JS idea is to separate business logic into a pure function (main())
+// and effects into Drivers
+// Cycle JS's run() function takes main and the dirvers as inputs
+// and runs the App. There is a slight trick to solve the chicken and egg
+// problem of circular relationship between main and drivers
+
+// main() takes sources as inputs and returns sinks
 const main = (sources) => {
   const click$ = sources.DOM
   return {
@@ -15,12 +26,19 @@ const main = (sources) => {
 }
 
 function domDriver(text$) {
-  text$subscribe({
+  //********************************************************************
+  // 1. subscribe to Application stream and use the data to update DOM
+  //********************************************************************
+  text$.subscribe({
     next: str => {
       const elem = document.querySelector('#app')
-      elem.textContect = str;
+      elem.textContect = str; // Update DOM
     }
   })
+  //********************************************************************
+  // 2. Return a stream that Application can subscribe to for
+  //    events from the DOM (User)
+  //********************************************************************
   const domSource = fromEvent(document, 'click')
   return domSource;
 }
